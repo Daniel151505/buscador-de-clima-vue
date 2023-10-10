295dc0085a9e43ecd15da81bfa2f729ef47eedc0
Daniel151505 <daniel150520011@gmail.com>
2023-10-09 19:41:00 -0500

    Creando el formulario principal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+import FormularioVue from "./components/Formulario.vue";
 </script>
 
 <template>
-
+  <h1 class="titulo">Buscador de Clima</h1>
+  <div class="contenedor buscador-clima">
+    <FormularioVue />
+  </div>
 </template>
