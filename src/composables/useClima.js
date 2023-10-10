c0897c594c71f07eddf40a13e1ed9ee5a49d5ea7
Daniel151505 <daniel150520011@gmail.com>
2023-10-09 20:33:01 -0500

    Primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -3,7 +3,9 @@ export default function useClima() {
     const obtenerClima = ({ciudad, pais}) => {
         // Importar el Api key
         const key = import.meta.env.VITE_API_KEY
+
         // Obtener la latitud y la longitud
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
         // Obtener el clima
     }
